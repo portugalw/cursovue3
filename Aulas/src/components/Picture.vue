093c80207ead8c3d964711f4f55e8fe640528577
Washington <portugalw@gmail.com>
2023-04-25 22:35:45 -0300

    Curso vue 24

---
+++ Aulas/src/components/Picture.vue
@@ -0,0 +1,38 @@
+<template>
+  <img :src="avatar" :alt="descricao" class="imagemPerfil">
+  <MudarImagem @mudar-imagem="trocaImagemPerfil"/>
+</template>
+<script>
+
+import MudarImagem from './MudarImagem.vue'
+
+export default {
+  name: 'Picture',
+  components: {
+    MudarImagem
+  },
+  data() {
+    return { 
+      avatar: "/img/avatar.png",
+      descricao: "Washington"
+    }
+  },
+  methods : {
+    trocaImagemPerfil () {
+      if(this.avatar == "/img/avatar.png"){
+        this.avatar = "/img/avatar2.png";
+      }
+      else{
+        this.avatar = "/img/avatar.png";
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.imagemPerfil {
+  width: 150px;
+}
+
+</style>
---
+++ ProjetoBurger/projetoburger/src/components/BurgerForm.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <p>FORM</p>
+        <div>
+            <form  id="burger-form">
+                <div class="input-container">
+                    <label for="nome" >Nome do cliente:</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
+                </div>
+                <div class="input-container">
+                    <label for="pao" >Escolha seu pão:</label>
+                    <select id="pao" name="pao" v-model="pao">
+                        <option value="">Selecione o pão</option>
+                        <option value="integral">Integral</option>
+                        <option value="frances">Francês</option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="carne" >Escolha carne:</label>
+                    <select id="carne" name="carne" v-model="carne">
+                        <option value="">Selecione carne</option>
+                        <option value="maminha">maminha</option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="opcionais" >Escolha carne:</label>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" id="opcionais" value="salame" v-model="opcionais">
+                        <span>salame</span>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Enviar " >
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'BurgerForm'
+    }
+</script>
+
+<style scoped>
+    #burger-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px ;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select {
+        padding: 5px;
+    }
+
+</style>
